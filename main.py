f9978826af26c6af3340b6769f479b82ca060211
cfrdm <cafererdem96@gmail.com>
2023-06-27 10:38:40 +0200

    all main değiştirildi

--- main.py
+++ main.py
@@ -5,17 +5,26 @@ from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
 import sys
 
+
+
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
 
+
+
 class LoginUI(QDialog):
     def __init__(self):
         super(LoginUI,self).__init__()
-        loadUi("./UI/login.ui",self)
+        loadUi("C:/Users/cfrdm/Desktop/ViT/Pomodoro Project/UI/login.ui",self)
+
+        self.signUpButton.clicked.connect(self.sign_up_button)
+
+        self.loginButton.clicked.connect(self.login_button)
+        
 
         # This is example of changing screen
-        self.loginButton.clicked.connect(self.go_main_menu)
+        
 
     def go_main_menu(self):
         main_menu = MainMenuUI()
@@ -35,38 +44,63 @@ class LoginUI(QDialog):
             print(f"The user named {name} has been successfully registered.")
         else:
             print("eksik veri girişi")
+        
+    def login_button(self):
 
+        db = sqlite3.connect("C://Users//cfrdm//Desktop//ViT//Pomodoro Project//Database//users.db")
+        im = db.cursor()
+        im.execute("Select* FROM users")
+
+        # print(im.fetchall())
+
+        login = self.emailInputLogin.text()
+        for i in im.fetchall():
+            im.execute("Select* FROM users")
+            if login in i: 
+                    self.go_main_menu()    
+                    # main_menu = MainMenuUI()
+                    # widget.addWidget(main_menu)
+                    # widget.setCurrentIndex(widget.currentIndex()+1)
+                # self.loginButton.clicked.connect(self.go_main_menu)
 
 class MainMenuUI(QDialog):
     def __init__(self):
         super(MainMenuUI,self).__init__()
-        loadUi("./UI/mainMenu.ui",self)
+        loadUi("C:/Users/cfrdm/Desktop/ViT/Pomodoro Project/UI/mainMenu.ui",self)
 
+        # recipients_add = self.addRecipientInput.text() kullanılacak kod
+        
 class PomodoroUI(QDialog):
     def __init__(self):
         super(PomodoroUI,self).__init__()
-        loadUi("./UI/pomodoro.ui",self)
+        loadUi("C:/Users/cfrdm/Desktop/ViT/Pomodoro Project/UI/pomodoro.ui",self)
+
+        self.goToMainMenuButton.clicked.connect(LoginUI.go_main_menu)
 
 
 class ShortBreakUI(QDialog):
     def __init__(self):
         super(ShortBreakUI,self).__init__()
-        loadUi("./UI/shortBreak.ui",self)
+        loadUi("C:/Users/cfrdm/Desktop/ViT/Pomodoro Project/UI/shortBreak.ui",self)
+
+        self.goToMainMenuButton.clicked.connect(LoginUI.go_main_menu)
 
 class LongBreakUI(QDialog):
     def __init__(self):
         super(LongBreakUI,self).__init__()
-        loadUi("./UI/longBreak.ui",self)
+        loadUi("C:/Users/cfrdm/Desktop/ViT/Pomodoro Project/UI/longBreak.ui",self)
+
+        self.goToMainMenuButton.clicked.connect(LoginUI.go_main_menu)
 
 
 app = QApplication(sys.argv)
 UI = LoginUI() # This line determines which screen you will load at first
 
 # You can also try one of other screens to see them.
-    # UI = MainMenuUI()
-    # UI = PomodoroUI()
-    # UI = ShortBreakUI()
-    # UI = LongBreakUI()
+# UI = MainMenuUI()
+# UI = PomodoroUI()
+# UI = ShortBreakUI()
+# UI = LongBreakUI()
 
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(UI)
@@ -75,3 +109,4 @@ widget.setFixedHeight(600)
 widget.setWindowTitle("Time Tracking App")
 widget.show()
 sys.exit(app.exec_())
+
